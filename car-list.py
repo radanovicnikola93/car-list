e28e2e018bdac7620ced167093bdd43ce6743f9c
Nikola <radanovicnikola93@gmail.com>
2018-02-04 14:31:56 +0100

    Added two new functions to add and choose cars

--- car-list.py
+++ car-list.py
@@ -28,15 +28,37 @@ def car_list(cars):
         for index, car in enumerate(cars):
             print "%s) %s %s with %s km driven so far. Last service date: %s" % (index+1, car.car_manufacturer, car.car_model,
                                                                                  car.kilometres_driven, car.last_service)
-def create_vehicle_object(c_manufacturer, c_model, km_driven_str, service, cars):
+def create_car_object(c_manufacturer, c_model, km_driven_str, service, cars):
     try:
         km_driven_str = km_driven_str.replace(",", ".")
         km_driven = float(km_driven_str)
 
         new_car = Car(c_manufacturer=c_manufacturer, c_model=c_model, km_driven=km_driven, service=service)
-
         cars.append(new_car)
 
         return True
     except ValueError:
         return False
+
+def add_new_car(cars):
+    c_manufacturer = raw_input("Please enter the car manufacturer: ")
+    c_model = raw_input("Please enter the model of the car: ")
+    km_driven_str = raw_input("Please enter the amount of kilometers that vehicle has done so far (just a number): ")
+    service = raw_input("Please enter the last service date (DD.MM.YYYY): ")
+
+    result = create_car_object(c_manufacturer, c_model, km_driven_str, service, cars)
+
+    if result:
+        print "You have successfully added %s %s as a new vehicle!" % (c_manufacturer, c_model)
+    else:
+        print "Please enter just a number for the kilometers done so far."
+
+
+def choose_car(cars):
+    print "Please choose the number of the vehicle that you would like to edit."
+    print ""
+    car_list(cars)
+    print ""
+    selection = raw_input("What vehicle number would you like to choose? ")
+    return cars[int(selection) - 1]
+
